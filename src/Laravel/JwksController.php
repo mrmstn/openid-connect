eae7a7c98b424cb7db752a4c227ae60c7e00f761
Michael Ramstein <michael@ramste.in>
2024-02-12 15:42:33 +0100

    Adds kid for available keys

--- src/Laravel/JwksController.php
+++ src/Laravel/JwksController.php
@@ -7,32 +7,62 @@ use Laravel\Passport\Passport;
 
 class JwksController
 {
-    public function jwks() {
+    public function jwks()
+    {
         $publicKey = $this->getPublicKey();
 
         // Source: https://www.tuxed.net/fkooman/blog/json_web_key_set.html
         $keyInfo = openssl_pkey_get_details(openssl_pkey_get_public($publicKey));
+        $keyDetails = [
+            'kty' => 'RSA',
+            'n' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($keyInfo['rsa']['n'])), '='),
+            'e' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($keyInfo['rsa']['e'])), '='),
+        ];
+        $keyDetails = $this->addKid($keyDetails);
 
         $jsonData = [
             'keys' => [
-                [
-                    'kty' => 'RSA',
-                    'n' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($keyInfo['rsa']['n'])), '='),
-                    'e' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($keyInfo['rsa']['e'])), '='),
-                ],
+                $keyDetails
             ],
         ];
 
         return response()->json($jsonData, 200, [], JSON_PRETTY_PRINT);
     }
 
-    private function getPublicKey(): string {
+    private function getPublicKey(): string
+    {
         $publicKey = str_replace('\\n', "\n", app()->make(Config::class)->get('passport.public_key') ?? '');
 
         if (!$publicKey) {
-            $publicKey = 'file://'.Passport::keyPath('oauth-public.key');
+            $publicKey = 'file://' . Passport::keyPath('oauth-public.key');
         }
 
         return $publicKey;
+    }
+
+
+    private function addKid(array $jwk): array
+    {
+        $thumbprint = $this->getJwkThumbprint($jwk);
+        $jwk['kid'] = $thumbprint;
+        return $jwk;
+    }
+
+    /**
+     * @param array $jwk
+     * @return string
+     *
+     * @see https://datatracker.ietf.org/doc/html/rfc7638
+     */
+    private function getJwkThumbprint(array $jwk): string
+    {
+        ksort($jwk); // Sort the keys to ensure that we generate always the same json.
+        $canonicalJwk = json_encode($jwk, JSON_UNESCAPED_SLASHES);
+
+        // Compute the SHA-256 hash of the canonical JWK
+        $hash = hash('sha256', $canonicalJwk, true);
+
+        // Base64url encode
+        return str_replace(array('+', '/', '='), array('-', '_', ''), base64_encode($hash));
     }
 }
